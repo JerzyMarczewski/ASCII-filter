3bd78632ba75aae77416759784cafbafe4329d8f
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-07-09 22:09:49 +0200

    added checking if webcam is allowed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import FilterViewer from "./components/FilterViewer";
 
 function App() {
+  const [webcamIsAllowed, setWebcamIsAllowed] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkWebcamAccess = async () => {
+      try {
+        await navigator.mediaDevices.getUserMedia({ video: true });
+        setWebcamIsAllowed(true);
+      } catch (error) {
+        setWebcamIsAllowed(false);
+      }
+    };
+
+    void checkWebcamAccess();
+  }, []);
+
+  if (webcamIsAllowed === null) return <div>Checking webcam access...</div>;
+
+  if (!webcamIsAllowed)
+    return (
+      <div>Webcam isn't allowed. Please allow to use the application.</div>
+    );
+
   return (
     <>
       <FilterViewer />
