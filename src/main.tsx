2403d2ff0c1e3dbd9f84c3e292ace0bce67cdad0
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-08-06 13:29:07 +0200

    added the store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux/es/exports";
 import App from "./App.tsx";
 import "./styles/index.css";
+import store from "./app/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
