e8ffe21586452d3bdf97e1cd3baa051731773d70
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-08-07 22:05:30 +0200

    added a filter property to the state and filter changing reducers

--- src/features/appStatusSlice.ts
+++ src/features/appStatusSlice.ts
@@ -2,15 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 type statusType = "default" | "loading" | "reload" | "final";
 export type webcamDimensionsType = { x: number; y: number };
+type filterType = "normal" | "ascii" | "pixelized";
+
+const allFilters: filterType[] = ["normal", "ascii", "pixelized"];
 
 interface appStatusState {
   status: statusType;
   webcamDimensions: webcamDimensionsType | undefined;
+  filter: filterType;
 }
 
 const initialState: appStatusState = {
   status: "default",
   webcamDimensions: undefined,
+  filter: "normal",
 };
 
 const appStatusSlice = createSlice({
@@ -23,9 +28,23 @@ const appStatusSlice = createSlice({
     setWebcamDimensions(state, actions: { payload: webcamDimensionsType }) {
       state.webcamDimensions = actions.payload;
     },
+    setPreviousFilter(state) {
+      const index = allFilters.indexOf(state.filter);
+      const newIndex = index === 0 ? allFilters.length - 1 : index - 1;
+      state.filter = allFilters[newIndex];
+    },
+    setNextFilter(state) {
+      const index = allFilters.indexOf(state.filter);
+      state.filter = allFilters[(index + 1) % allFilters.length];
+    },
   },
 });
 
-export const { setAppStatus, setWebcamDimensions } = appStatusSlice.actions;
+export const {
+  setAppStatus,
+  setWebcamDimensions,
+  setPreviousFilter,
+  setNextFilter,
+} = appStatusSlice.actions;
 
 export default appStatusSlice.reducer;
