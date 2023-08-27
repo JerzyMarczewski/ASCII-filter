e5904886a01c496365466fd387579092abaf4880
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-08-27 16:49:12 +0200

    changed structure

--- src/components/FilterViewer.tsx
+++ src/components/FilterViewer.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Icon } from "@iconify/react";
 import "../styles/FilterViewer.css";
 import CameraViewer from "./CameraViewer";
+import SettingsPanel from "./SettingsPanel";
 
 function FilterViewer() {
   const filter = useSelector((state: RootState) => state.appStatus.filter);
@@ -11,21 +12,27 @@ function FilterViewer() {
   const dispatch = useDispatch();
 
   return (
-    <>
-      <CameraViewer />
-      <div className="filterOptions">
-        <div
-          className="arrowWrapper"
-          onClick={() => dispatch(setPreviousFilter())}
-        >
-          <Icon className="arrow" icon="ic:round-arrow-left" />
-        </div>
-        <div className="chosenFilterDisplay">{filter}</div>
-        <div className="arrowWrapper" onClick={() => dispatch(setNextFilter())}>
-          <Icon className="arrow" icon="ic:round-arrow-right" />
+    <div>
+      <div>
+        <CameraViewer />
+        <div className="filterOptions">
+          <div
+            className="arrowWrapper"
+            onClick={() => dispatch(setPreviousFilter())}
+          >
+            <Icon className="arrow" icon="ic:round-arrow-left" />
+          </div>
+          <div className="chosenFilterDisplay">{filter}</div>
+          <div
+            className="arrowWrapper"
+            onClick={() => dispatch(setNextFilter())}
+          >
+            <Icon className="arrow" icon="ic:round-arrow-right" />
+          </div>
         </div>
       </div>
-    </>
+      <SettingsPanel />
+    </div>
   );
 }
 
